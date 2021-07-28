6471c0501ac37593bf91943e3ddbd286bdea9aa1
Brian Sai <brianwsai@gmail.com>
2021-07-28 13:34:20 -0700

    Implemented logic for main to consume published data from admin

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,6 @@
-import pika
+import pika, json
+
+from main import Product, db
 from decouple import config
 
 params = pika.URLParameters(config('AMQP_URL'))
@@ -7,13 +9,35 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='admin')
+channel.queue_declare(queue='main')
+
 
 def callback(ch, method, properties, body):
-  print('Received in admin')
-  print(body)
+    print('Received in main')
+    data = json.loads(body)
+    print(data)
 
-channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
+
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
