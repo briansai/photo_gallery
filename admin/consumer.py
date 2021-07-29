641a93043b3fd5167a2ed1b73bd554210aed5941
Brian Sai <brianwsai@gmail.com>
2021-07-29 12:46:03 -0700

    Implemented backend api for liking an image

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,4 +1,9 @@
-import pika
+import pika, json, os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
 from decouple import config
 
 params = pika.URLParameters(config('AMQP_URL'))
@@ -11,7 +16,11 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
   print('Received in admin')
-  print(body)
+  id = json.loads(body)
+  product = Product.objects.get(id=id)
+  product.likes += 1
+  product.save()
+  print('Product likes increased by 1')
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 from decouple import config
+from producer import publish
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}/main"
@@ -33,6 +35,24 @@ class ProductUser(db.Model):
 @app.route('/api/products')
 def index():
   return jsonify(Product.query.all())
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+  req = requests.get('http://docker.for.mac.localhost:8000/api/user')
+  json = req.json()
+
+  try:
+    productUser = ProductUser(user_id=json['id'], product_id=id)
+    db.session.add(productUser)
+    db.session.commit()
+
+    publish('product_liked', id)
+  except:
+    abort(400, 'You already liked this product')
+
+  return jsonify({
+    'message': 'success'
+  })
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
